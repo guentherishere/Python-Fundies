61953ece84951fb739c8ae6e10e44df52074a295
Matt Guenther <mguenther006@users.noreply.github.com>
2017-10-30 16:58:06 -0500

    First Commit
    First Commit

---
+++ variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+x=1
+print(x)
+# x='foo'
+print(x)
+#strings and integers don't match
+x=x+1
+print(x)
+#diff between python 2 and python 3 (raw_input)
+y=input("Hi there. Type something.")
+print(y)
